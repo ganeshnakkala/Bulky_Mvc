586aa06325d2eee8c4dc9ff0a67a59174b687ac2
Ganesh Nakkala <ganeshffo@gmail.com>
2024-03-29 09:20:54 -0500

    Implemented image upload functionality for product creation in ProductController
    - Created logic to handle file upload and save images in the specified directory structure.
    - Injected IWebHostEnvironment to access the web root path.
    - Handled file renaming to a random GUID with the original file extension.
    - Updated the ProductViewModel's ImageUrl property with the correct image path.
    - Resolved an issue with file upload by ensuring the file input field has a name attribute.
    - Verified functionality by testing product creation with image upload.
    - Added debugging and validation to ensure proper file upload and storage.

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,8 @@
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
+using Elfie.Serialization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
@@ -13,9 +15,11 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     {
         
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork) 
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment) 
         { 
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -58,12 +62,23 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
+
             {
+               string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString()+Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product\");
+                    using ( var fileStream = new FileStream(Path.Combine(fileName, productPath), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
                 _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["Success"] = "Product created Successfully";
                 return RedirectToAction("Index");
-
             }
             else
             {
